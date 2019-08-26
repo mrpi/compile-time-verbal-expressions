a2c7daa87731a252383c76c7b168c23e5ad1141e
Ludger Sprenker <ludger@sprenker.net>
2019-08-26 20:34:07 +0200

    Workaround MSVC problems with connstexpr meta-functions

--- include/ctve.hpp
+++ include/ctve.hpp
@@ -286,16 +286,32 @@ namespace impl
 
 namespace impl
 {
-  template <size_t BufLen1, size_t BufLen2>
-  constexpr auto sanitized_str_or_pattern(const char (&prefix)[BufLen1],
-                                          const char (&suffix)[BufLen2])
+  template <size_t Len1, size_t Len2>
+  struct sanitized_str_or_pattern
   {
-    return [prefix, suffix](auto&& buf) {
+    static_string<Len1> prefix;
+    static_string<Len2> suffix;
+
+    template <size_t BufLen1, size_t BufLen2>
+    constexpr sanitized_str_or_pattern(const char (&prefix)[BufLen1],
+                                       const char (&suffix)[BufLen2])
+     : prefix{prefix}, suffix{suffix}
+    {
+    }
+
+    template <typename T>
+    constexpr auto operator()(T&& buf) const
+    {
       static_assert(!std::is_same_v<range, std::decay_t<decltype(buf)>>,
                     "Only string literals allowed!");
       return pattern{static_string{prefix + sanitize(buf) + suffix}};
-    };
-  }
+    }
+  };
+
+  template <size_t BufLen1, size_t BufLen2>
+  sanitized_str_or_pattern(const char (&prefix)[BufLen1],
+                           const char (&suffix)[BufLen2])
+      ->sanitized_str_or_pattern<BufLen1 - 1, BufLen2 - 1>;
 
   template <size_t Len1, size_t Len2>
   struct sanitized_str_or_range
@@ -336,18 +352,33 @@ namespace impl
                          const char (&suffix)[BufLen2])
       ->sanitized_str_or_range<BufLen1 - 1, BufLen2 - 1>;
 
-  template <size_t BufLen1, size_t BufLen2>
-  constexpr auto sanitized_str(const char (&prefix)[BufLen1],
-                               const char (&suffix)[BufLen2])
+  template <size_t Len1, size_t Len2>
+  struct sanitized_str
   {
-    return [prefix, suffix](auto&& buf) {
+    static_string<Len1> prefix;
+    static_string<Len2> suffix;
+
+    template <size_t BufLen1, size_t BufLen2>
+    constexpr sanitized_str(const char (&prefix)[BufLen1],
+                            const char (&suffix)[BufLen2])
+     : prefix{prefix}, suffix{suffix}
+    {
+    }
+
+    template <typename T>
+    constexpr auto operator()(T&& buf) const
+    {
       static_assert(!std::is_same_v<range, std::decay_t<decltype(buf)>>,
                     "Only string literals allowed!");
       static_assert(!is_pattern<std::decay_t<decltype(buf)>>::value,
                     "Only string literals allowed!");
       return pattern{static_string{prefix + sanitize(buf) + suffix}};
-    };
-  }
+    }
+  };
+
+  template <size_t BufLen1, size_t BufLen2>
+  sanitized_str(const char (&prefix)[BufLen1], const char (&suffix)[BufLen2])
+      ->sanitized_str<BufLen1 - 1, BufLen2 - 1>;
 } // namespace impl
 
 static inline constexpr auto find = impl::sanitized_str("(?:", ")");
